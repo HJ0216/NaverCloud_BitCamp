af413387fc7652677154289b49da7041554373d8
KimHyunji <119024959+HJ0216@users.noreply.github.com>
2023-03-13 10:04:28 +0900

    add files on nested pkg

---
+++ JAVA/09_nested/nested/AbstractTest.java
@@ -0,0 +1,11 @@
+package nested;
+
+public abstract class AbstractTest { // POJO 형식
+	protected String name;
+	
+	public abstract void setName(String name);
+	
+	public String getName() {return name;}
+	
+	
+}
