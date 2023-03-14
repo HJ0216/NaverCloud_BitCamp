50b0a8f7b3d7303addd6cf8d2778c702a03dbc3c
KimHyunji <119024959+HJ0216@users.noreply.github.com>
2023-03-14 10:11:21 +0900

    add files on io pkg

---
+++ JAVA/12_io/io/PersonDTO.java
@@ -0,0 +1,25 @@
+package io;
+
+import java.io.Serializable;
+
+public class PersonDTO implements Serializable { // abstract method X
+
+	private String name;
+	private int age;
+	private double height;
+
+	public PersonDTO(String name, int age, double height) {
+		this.name = name;
+		this.age = age;
+		this.height = height;
+	}
+	
+	public void setName(String name) {this.name = name;}
+	public void setAge(int age) {this.age = age;}
+	public void setHeight(double height) {this.height = height;}
+	
+	public String getName() {return name;}
+	public int getAge() {return age;}
+	public double getHeight() {return height;}
+	
+}
